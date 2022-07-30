c52631080d7540a9aa15f5a6f19697e9fb25b884
Ryan Walpole <ryan.walpole@outlook.com>
2022-07-30 13:55:14 +1000

    Update Program.cs
    Add syncfusion license
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Source Code/Astroneer Launcher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Astroneer_Launcher
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Njg2Nzc3QDMyMzAyZTMyMmUzMGhqaENZb20zK1dmYys5QlltZjZzMUtLN1BHSEV1aStubTFVMmhJUExMbUk9");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Splash());
+        }
+    }
+}
