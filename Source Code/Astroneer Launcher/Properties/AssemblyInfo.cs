dc1d0a7d5f35fb05a5574d2cd6ba8425007650dc
Ryan Walpole <ryan.walpole@outlook.com>
2022-08-02 15:03:02 +1000

    Create Assembly
    Create assembly information for release
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Source Code/Astroneer Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Astroneer Launcher")]
+[assembly: AssemblyDescription("Keep your save files backed up by launching Astroneer for Steam through the Astroneer launcher.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RWE Labs, a subsidiary of Ryan Walpole Enterprises")]
+[assembly: AssemblyProduct("Astroneer Launcher")]
+[assembly: AssemblyCopyright("Copyright © RWE Labs, Ryan Walpole Enterprises - 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c6b66722-9122-42b1-ad20-b1f55e109697")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("22.08.0.1")]
+[assembly: AssemblyFileVersion("22.08.0.1")]
+[assembly: NeutralResourcesLanguage("en-AU")]
