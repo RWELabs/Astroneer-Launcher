6739034d9b89fecbd17794cc3fd549807779c1a2
Ryan Walpole <ryan.walpole@outlook.com>
2022-08-03 09:39:40 +1000

    Create Window - Download (UI)
    Create user interface for download window.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Source Code/Astroneer Launcher/Windows/Update/Download.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Astroneer_Launcher.Windows.Update
+{
+    partial class Download
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Download));
+            this.PercentText = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Progress = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.StartExecute = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PercentText
+            // 
+            this.PercentText.Location = new System.Drawing.Point(14, 87);
+            this.PercentText.Name = "PercentText";
+            this.PercentText.Size = new System.Drawing.Size(269, 14);
+            this.PercentText.TabIndex = 43;
+            this.PercentText.Text = "PercentText";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(72, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(264, 23);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Astroneer Launcher";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(75, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(287, 16);
+            this.label7.TabIndex = 41;
+            this.label7.Text = "Downloading update...";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Progress
+            // 
+            this.Progress.Location = new System.Drawing.Point(17, 110);
+            this.Progress.Name = "Progress";
+            this.Progress.Size = new System.Drawing.Size(290, 24);
+            this.Progress.TabIndex = 38;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F);
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(75, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 16);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "labs.ryanwalpole.com";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Astroneer_Launcher.Properties.Resources.AstroneerLauncher;
+            this.pictureBox5.Location = new System.Drawing.Point(16, 19);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(53, 51);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 40;
+            this.pictureBox5.TabStop = false;
+            // 
+            // StartExecute
+            // 
+            this.StartExecute.Interval = 3000;
+            this.StartExecute.Tick += new System.EventHandler(this.StartExecute_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Astroneer_Launcher.Properties.Resources.StayUpdated;
+            this.pictureBox1.Location = new System.Drawing.Point(246, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 44;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Download
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(336, 155);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.PercentText);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.Progress);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Download";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Update Software | RWE Labs";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label PercentText;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.ProgressBar Progress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer StartExecute;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
