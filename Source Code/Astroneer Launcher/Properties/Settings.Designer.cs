af7bc1b0b451daface95d913d4787988411a9378
Ryan Walpole <ryan.walpole@outlook.com>
2023-01-11 11:13:10 +1100

    Update Version Number
    Update version number in internal settings to 230101.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- Source Code/Astroneer Launcher/Properties/Settings.Designer.cs
+++ Source Code/Astroneer Launcher/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Astroneer_Launcher.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Astroneer_Launcher.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("220803")]
+        [global::System.Configuration.DefaultSettingValueAttribute("230101")]
         public string VersionNumber {
             get {
                 return ((string)(this["VersionNumber"]));
